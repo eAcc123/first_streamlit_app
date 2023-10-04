750e14fb31613a3e3d3774fdf85dde2ab0d904fe
eAcc123 <146936358+eAcc123@users.noreply.github.com>
2023-10-04 17:50:46 +0200

    Update streamlit_app.py
    Cleaning api code for better flow control

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,11 @@
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+
+from urllib.error import URLError
+
+
 
 streamlit.title('My parents New Healthy Diner')
 
@@ -10,7 +17,7 @@ streamlit.text('🥑🍞Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
+#PANDAS
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -21,20 +28,28 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+#FRUITYVICE API
+#Api call and data display
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+this_fruit_choice)
+  #Leverage pandas to normalize json output
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
+
 #Header for new API section
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    back_from_f = get_fruityvice_data(fruit_choice)
+    #Place data into a table
+    streamlit.dataframe(back_from_f)
+except URLError as e:
+  streamlit.error()
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-
-#Leverage pandas to normalize json output
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-#Place data into a table
-streamlit.dataframe(fruityvice_normalized)
-
-import snowflake.connector
+#SNOWFLAKE
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
